fbb7d1acc92c942d26499ffc46d8b7513e21ee91
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-17 07:47:03 -0200

    Constantize and document the regular expressions used within the option parser.

--- lib/acclaim/option/parser.rb
+++ lib/acclaim/option/parser.rb
@@ -6,6 +6,32 @@ module Acclaim
     # Parses arrays of strings and returns an Options instance containing data.
     class Parser
 
+      # Regular expression for any kind of option switch.
+      #
+      # Matches strings that begin with 1 or 2 dashes, are followed by at least
+      # one word character or number, and may be followed by any other word
+      # character, number or dash.
+      #
+      # Examples: -s, --long, -multiple, -1, --no-feature, --with_underscore,
+      #           --_private_option, etc.
+      SWITCH = /^-{1,2}[\w\d]+[\w\d-]*$/
+
+      # Regular expression for multiple short options in a single "short"
+      # switch.
+      #
+      # Matches strings that begin with a single dash and are followed by 2 or
+      # more word characters, among which is the underscore but not the dash
+      # character.
+      #
+      # Examples: -xvf, -abc, -de_f
+      MULTIPLE_SHORT_SWITCHES = /^-\w{2,}$/
+
+      # Regular expression for the string that separates options and their
+      # parameters from arguments like filenames.
+      #
+      # Matches strings made up of 2 or more dashes.
+      ARGUMENT_SEPARATOR = /^-{2,}$/
+
       class Error < StandardError
 
         def self.raise_wrong_arg_number(actual, minimum, optional)
@@ -46,7 +72,7 @@ module Acclaim
       end
 
       def split_multiple_short_options!
-        argv.find_all { |arg| arg =~ /^-\w{2,}/ }.each do |multiples|
+        argv.find_all { |arg| arg =~ MULTIPLE_SHORT_SWITCHES }.each do |multiples|
           multiples_index = argv.index multiples
           argv.delete multiples
           letters = multiples.sub!(/^-/, '').split(//)
@@ -80,7 +106,7 @@ module Acclaim
                   values = []
                   params.each do |param|
                     case param
-                      when nil, /^(-{1,2}|-{2,}$)/ then break
+                      when nil, SWITCH, ARGUMENT_SEPARATOR then break
                       else
                         break if optional >= 0 and values.count >= minimum + optional
                         values << param
