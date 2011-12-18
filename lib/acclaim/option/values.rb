090f3fe71cb81956e5178e95abcf651cff3142c3
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-18 11:14:59 -0200

    Update Option::Values documentation.

--- lib/acclaim/option/values.rb
+++ lib/acclaim/option/values.rb
@@ -4,23 +4,26 @@ module Acclaim
     # Represents a set of option values.
     class Values
 
+      # Gets a value by key.
       def [](key)
         data[key]
       end
 
+      # Sets a value by key.
       def []=(key, value)
         data[key] = value
       end
 
+      # Merge these values with the others.
       def merge!(other, &block)
         data.merge! other.data, &block
       end
 
       # Handles the following cases:
       #
-      #   options.method = value
-      #   options.method?
-      #   options.method
+      #   options.method = value  =>  options[method] = value
+      #   options.method?         =>  options[method] ? true : false
+      #   options.method          =>  options[method]
       def method_missing(method, *args, &block)
         m = method.to_s.chop!.to_sym
         case method
