fdb201e3421ebaf86c064678b624ac928b69f19f
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-19 02:20:21 -0200

    Add fairly simple implementation of a generic help command module.

---
+++ lib/acclaim/command/help.rb
@@ -0,0 +1,41 @@
+module Acclaim
+  class Command
+
+    # Module which adds help support to a command.
+    module Help
+
+      def self.create(base, opts = {})
+        base.option :help, '-h', '--help', 'Show usage information and exit.'
+        base.const_set(:Help, Class.new(base)).tap do |help_command|
+          help_command.when_called do |options, args|
+            display_help_for base.root
+            exit
+          end
+        end
+      end
+
+      # Displays a very simple help screen for the given command and all its
+      # subcommands.
+      def self.display_help_for(command)
+        # TODO rewrite this VERY CRUDE implementation.
+        # Look into how to code a text formatter later.
+        help_string = ''
+        command.options.tap do |options|
+          if options.any?
+            help_string << "\nCommand '#{command.line}':\n\n" unless command.root?
+            max = options.map { |option| option.names.join(', ').length }.max
+            options.each do |option|
+              switches = option.names.join ', '
+              help_string << ' ' * 4 << switches << ' ' * (4 + max - switches.length)
+              help_string << option.description << "\n"
+            end
+          end
+        end
+        puts help_string unless help_string.empty?
+        command.subcommands.each { |subcommand| display_help_for subcommand }
+      end
+
+    end
+
+  end
+end
