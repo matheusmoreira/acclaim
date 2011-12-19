99344862bbc8ee1964d413da6603d82fc024926b
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-18 23:42:45 -0200

    Improve Parser spec.

--- spec/acclaim/option/parser_spec.rb
+++ spec/acclaim/option/parser_spec.rb
@@ -7,121 +7,162 @@ describe Acclaim::Option::Parser do
   let(:options) { nil }
   subject { Acclaim::Option::Parser.new(args, options) }
 
-  context 'when given a long switch with a parameter separated by an equals sign' do
-
-    let!(:args) { %w(--switch=PARAM) }
-
-    describe '#parse!' do
+  describe '#parse!' do
+    context 'when given a long switch with a parameter separated by an equals sign' do
+      let!(:args) { %w(--switch=PARAM) }
 
       it 'should separate the switch from the single parameter' do
         subject.parse!
         args.should == %w(--switch PARAM)
       end
-
     end
 
-  end
-
-  context 'when given a long switch with multiple parameters separated by an equals sign' do
-
-    let!(:args) { %w(--files FILE1 FILE2 FILE3) }
-
-    describe '#parse!' do
+    context 'when given a long switch with multiple parameters separated by an equals sign' do
+      let!(:args) { %w(--files FILE1 FILE2 FILE3) }
 
       it 'should separate the switch and the parameters' do
         subject.parse!
         args.should == %w(--files FILE1 FILE2 FILE3)
       end
-
     end
 
-  end
+    context 'when given multiple combined short options' do
+      let!(:args) { %w(-abcd) }
 
-  describe '#parse!' do
-
-    let!(:args) do
-      %w(cmd -a subcmd -b PARAM1 -cdef PARAM2 --long --parameters PARAM3 PARAM4 PARAM5 -- FILE1 FILE2)
-    end
-
-    it 'should split multiple short options' do
-      new_argv = args.dup.tap do |args|
-        args[args.index('-cdef')] = %w[-c -d -e -f]
-      end.flatten!
-      subject.parse!
-      args.should == new_argv
+      it 'should split the combined option into multiple short options' do
+        subject.parse!
+        args.should == %w(-a -b -c -d)
+      end
     end
 
     context 'when not given an array of options' do
-
-      it 'should not return an options instance' do
+      it 'should not return any option values' do
         subject.parse!.should be_nil
       end
-
     end
 
     context 'when given an array of options' do
+      let(:options) { [ Acclaim::Option.new(:option) ] }
 
-      let(:options) do
-        [].tap do |opts|
-          ('a'..'f').each do |c|
-            hash = {}
-            hash[:arity] = [1, 0] if c == 'b' or c == 'f'
-            hash[:required] = true if c == 'd'
-            opts << Acclaim::Option.new(c.to_sym, "-#{c}", hash)
-          end
-          opts << Acclaim::Option.new(:long, '--long')
-          opts << Acclaim::Option.new(:params, '--parameters', arity: [1, 1], default: [])
-        end
-      end
-
-      subject { Acclaim::Option::Parser.new(args, options) }
-
-      it 'should return an options instance' do
+      it 'should return option values' do
         subject.parse!.should_not be_nil
       end
 
-      it 'should parse arguments correctly' do
-        subject.parse!.instance_eval do
-          a?.should be_true
-          b.should == 'PARAM1'
-          c?.should be_true
-          d?.should be_true
-          e?.should be_true
-          f.should == 'PARAM2'
-          long?.should be_true
-          params.should == %w(PARAM3 PARAM4)
+      context 'containing a required option' do
+        let(:options) { [ Acclaim::Option.new(:option, '-o', required: true) ] }
+
+        context 'but not given the required switch' do
+          let!(:args) { [] }
+
+          it 'should raise an error' do
+            expect { subject.parse! }.to raise_error Acclaim::Option::Parser::Error, /required/
+          end
+        end
+
+        context 'and given the required switch' do
+          let!(:args) { %w(-o) }
+
+          it 'parse the value of the option correctly' do
+            subject.parse!.option?.should be_true
+          end
         end
       end
 
-      it 'should leave unparsed arguments in argv' do
-        subject.parse!
-        args.should == %w(cmd subcmd PARAM5 -- FILE1 FILE2)
-      end
+      context 'containing an option with a required parameter' do
+        let(:options) { [ Acclaim::Option.new(:option, '-o', arity: [1,0]) ] }
 
-      context 'but not given a required parameter' do
+        context "and not given the option's switch" do
+          let!(:args) { [] }
 
-        let!(:args) { %w(-db) }
-
-        it 'should raise an error' do
-          expect { subject.parse! }.to raise_error Acclaim::Option::Parser::Error,
-                                                   /number of arguments/
+          it 'should not raise an error' do
+            expect { subject.parse! }.to_not raise_error Acclaim::Option::Parser::Error, /arguments/
+          end
         end
 
+        context "and given the option's switch" do
+          context 'with a parameter' do
+            let!(:args) { %w(-o PARAM) }
+
+            it 'should not raise an error' do
+              expect { subject.parse! }.to_not raise_error Acclaim::Option::Parser::Error, /arguments/
+            end
+
+            it 'should parse the value of the parameter correctly' do
+              subject.parse!.option.should == 'PARAM'
+            end
+          end
+
+          context 'without a parameter' do
+            let!(:args) { %w(-o) }
+
+            it 'should raise an error' do
+              expect { subject.parse! }.to raise_error Acclaim::Option::Parser::Error, /arguments/
+            end
+          end
+        end
       end
 
-      context 'but not passed a required option' do
+      context 'containing an option with an optional argument' do
+        let(:options) { [ Acclaim::Option.new(:volume, '-V', arity: [0,1], default: 2) ] }
 
-        let!(:args) { [] }
+        context 'and not given the option a parameter' do
+          let!(:args) { %w(-V) }
 
-        it 'should raise an error' do
-          expect { subject.parse! }.to raise_error Acclaim::Option::Parser::Error,
-                                                   /required/
+          it 'should not raise an error' do
+            expect { subject.parse! }.to_not raise_error Acclaim::Option::Parser::Error, /arguments/
+          end
+
+          it 'should initialize the option to its default' do
+            subject.parse!.volume.should == 2
+          end
         end
 
+        context 'and given the option a parameter' do
+          let!(:args) { %w(-V 5) }
+
+          it 'should not raise an error' do
+            expect { subject.parse! }.to_not raise_error Acclaim::Option::Parser::Error, /arguments/
+          end
+
+          it 'should initialize the option to the value given' do
+            subject.parse!.volume.should == '5'
+          end
+        end
       end
 
+      context 'containing an option that requires at least one argument' do
+        let(:options) { [ Acclaim::Option.new(:files, '--files', arity: [1,-1]) ] }
+
+        context 'and not given the switch any arguments' do
+          let!(:args) { %w(--files) }
+
+          it 'should raise an error' do
+            expect { subject.parse! }.to raise_error Acclaim::Option::Parser::Error, /arguments/
+          end
+        end
+
+        context 'and given the switch three arguments' do
+          let!(:args) { %w(--files FILE1 FILE2 FILE3) }
+
+          it 'should not raise an error' do
+            expect { subject.parse! }.to_not raise_error Acclaim::Option::Parser::Error, /arguments/
+          end
+
+          it 'should initialize the option to the values given' do
+            subject.parse!.files.should == %w(FILE1 FILE2 FILE3)
+          end
+
+          context 'among other arguments and commands' do
+            let!(:args) { %w(cmd subcmd --files FILE1 FILE2 -- ARG1 ARG2) }
+
+            it 'should ignore the other arguments and leave them where they are' do
+              subject.parse!
+              args.should == %w(cmd subcmd -- ARG1 ARG2)
+            end
+          end
+        end
+      end
     end
-
   end
 
 end
