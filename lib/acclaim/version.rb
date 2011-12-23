759692cb7a8009206880ab3d46c7a2efa3c0970c
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-23 10:06:52 -0200

    Add documentation for Acclaim::Version.

--- lib/acclaim/version.rb
+++ lib/acclaim/version.rb
@@ -1,11 +1,30 @@
 module Acclaim
+
+  # Acclaim's version.
   module Version
 
+    # Major version.
+    #
+    # Increments denote backward-incompatible changes and additions.
     MAJOR = 0
+
+    # Minor version.
+    #
+    # Increments denote backward-compatible changes and additions.
     MINOR = 0
+
+    # Patch version.
+    #
+    # Increments denote changes in implementation.
     PATCH = 4
+
+    # Build version.
+    #
+    # Used for pre-release versions.
     BUILD = nil
 
+    # Complete version string, which is every individual version number joined
+    # by a dot (<tt>'.'</tt>), in descending order of prescedence.
     STRING = [ MAJOR, MINOR, PATCH, BUILD ].compact.join '.'
 
   end
