37bb5b942a9a1e952ba88d11bac20c7d0b62c27f
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-23 03:23:04 -0200

    Add option type handler manager.

---
+++ lib/acclaim/option/type.rb
@@ -0,0 +1,49 @@
+module Acclaim
+  class Option
+
+    # Associates a class with a handler block.
+    module Type
+
+      instance_eval do
+
+        # Yields class, proc pairs if a block was given. Returns an enumerator
+        # otherwise.
+        def each(&block)
+          table.each &block
+        end
+
+        # Returns all registered classes.
+        def all
+          table.keys
+        end
+
+        alias registered all
+
+        # Registers a handler for a class.
+        def register(klass, &block)
+          table[klass] = block
+        end
+
+        alias add_handler_for register
+        alias accept register
+
+        # Returns the handler for the given class.
+        def handler_for(klass)
+          table[klass]
+        end
+
+        alias [] handler_for
+
+        private
+
+        # The hash used to associate classes with their handlers.
+        def table
+          @table ||= {}
+        end
+
+      end
+
+    end
+
+  end
+end
