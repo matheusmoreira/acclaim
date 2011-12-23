e3963261732089206b996d2eb709f4112c5e86fd
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-23 10:04:45 -0200

    Add documentation for Command::Help::Template.

--- lib/acclaim/command/help/template.rb
+++ lib/acclaim/command/help/template.rb
@@ -7,11 +7,16 @@ module Acclaim
       # Manages help templates.
       module Template
 
+        # Loads an ERB template file from the
+        # +lib/acclaim/command/help/template+ folder and instantiates a new ERB
+        # instance with its contents.
         def self.load(template)
           filename = File.join File.dirname(__FILE__), 'template', template
           ERB.new File.read(filename), nil, '%<>'
         end
 
+        # Computes the result of the template +file+ using the +command+'s
+        # binding.
         def self.for(command, file = 'command.erb')
           template = self.load file
           b = command.instance_eval { binding }
