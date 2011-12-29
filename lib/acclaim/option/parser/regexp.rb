adc26a710f8f50b0284a4c0a1e6d93cb939429b5
Matheus Moreira <matheus-m@live.com>
2011-12-29 07:19:21 -0200

    Don't go to great lengths to explain what the Regexp matches. (DRY)

--- lib/acclaim/option/parser/regexp.rb
+++ lib/acclaim/option/parser/regexp.rb
@@ -7,9 +7,6 @@ module Acclaim
 
         # Regular expression for a short option switch.
         #
-        # Matches strings that begin with a single dash and contains only one
-        # word character before the end of the string.
-        #
         # Examples: <tt>-s; -_</tt>
         #
         # <tt>'-mult'</tt> should match MULTIPLE_SHORT_SWITCHES, and will be
@@ -19,11 +16,6 @@ module Acclaim
 
         # Regular expression for a long option switch.
         #
-        # Matches strings that begin with a double dash, contain one or more
-        # word character or digit, and can be followed by either nothing or a
-        # single dash. The latter must be followed by one or more word character
-        # or digit.
-        #
         # Examples: <tt>--long; --no-feature; --with_underscore;
         # --_private-option; --1-1</tt>
         LONG_SWITCH = /\A--[\w\d]+(-[\w\d]+)*\Z/
@@ -31,23 +23,11 @@ module Acclaim
         # Regular expression for multiple short options in a single "short"
         # switch.
         #
-        # Matches strings that begin with a single dash and are followed by 2 or
-        # more word characters, among which is the underscore but not the dash
-        # character.
-        #
         # Examples: -xvf, -abc, -de_f
         MULTIPLE_SHORT_SWITCHES = /\A-\w{2,}\Z/
 
         # Regular expression for a long switch connected to its parameters with
         # an equal sign. Multiple parameters are be separated by commas.
-        #
-        # Matches strings that begin with a double dash, are followed by one or
-        # more word character or digit and may be followed by one dash and one
-        # or more word character or digit.
-        #
-        # After that, there must be an equals sign, which must be followed by
-        # either nothing, any number of commmas or any number of word characters
-        # or digits.
         #
         # Examples:
         # <tt>
@@ -63,15 +43,12 @@ module Acclaim
         SWITCH_PARAM_EQUALS = /\A--[\w\d]+(-[\w\d]+)*=(,*[\w\d]*)*\Z/
 
         # Regular expression for any kind of option switch.
-        #
-        # Matches anything that matches any of the other switch regular
-        # expressions.
         SWITCH = /(#{SHORT_SWITCH})|(#{LONG_SWITCH})|(#{MULTIPLE_SHORT_SWITCHES})|(#{SWITCH_PARAM_EQUALS})/
 
         # Regular expression for the string that separates options and their
         # parameters from arguments like filenames.
         #
-        # Matches strings made up of 2 or more dashes.
+        # Examples: <tt>--; ---</tt>
         ARGUMENT_SEPARATOR = /\A-{2,}\Z/
 
       end
