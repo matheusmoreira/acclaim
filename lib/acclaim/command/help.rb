c65737033bb85bd0bd82a5a213e6126c4db652cc
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-01-18 16:34:13 -0200

    Put class methods under class << self.

--- lib/acclaim/command/help.rb
+++ lib/acclaim/command/help.rb
@@ -7,47 +7,52 @@ module Acclaim
     # Module which adds help support to a command.
     module Help
 
-      # Adds a special help option to the given +command+.
-      #
-      # The last argument can be a configuration hash, which accepts the
-      # following options:
-      #
-      # [:switches]  The switches used when creating the help option.
-      def self.add_options_to!(*args)
-        opts, command = args.extract_ribbon!, args.first
-        switches = opts.switches? %w(-h --help)
-        description = opts.description? 'Show usage information and exit.'
-        command.option :acclaim_help, *switches, description
-      end
+      # The class methods.
+      class << self
 
-      private_class_method :add_options_to!
-
-      # Creates a help subcommand that inherits from the given +base+ command
-      # and stores the class in the +Help+ constant of +base+. When called, the
-      # command displays a help screen including information for all commands
-      # and then exits.
-      #
-      # The last argument can be a configuration hash, which accepts the
-      # following options:
-      #
-      # [:options]   If +true+, will add a help option to the +base+ command.
-      # [:switches]  The switches used when creating the help option.
-      def self.create(*args)
-        opts, base = args.extract_ribbon!, args.first
-        add_options_to! base, opts if opts.options? true
-        base.const_set(:Help, Class.new(base)).tap do |help_command|
-          help_command.when_called do |options, args|
-            display_help_for base.root
-            exit
+        # Creates a help subcommand that inherits from the given +base+ command
+        # and stores the class in the +Help+ constant of +base+. When called, the
+        # command displays a help screen including information for all commands
+        # and then exits.
+        #
+        # The last argument can be a configuration hash, which accepts the
+        # following options:
+        #
+        # [:options]   If +true+, will add a help option to the +base+ command.
+        # [:switches]  The switches used when creating the help option.
+        def create(*args)
+          opts, base = args.extract_ribbon!, args.first
+          add_options_to! base, opts if opts.options? true
+          base.const_set(:Help, Class.new(base)).tap do |help_command|
+            help_command.when_called do |options, args|
+              display_help_for base.root
+              exit
+            end
           end
         end
-      end
 
-      # Displays a very simple help screen for the given command and all its
-      # subcommands.
-      def self.display_help_for(command)
-        puts Template.for(command) if command.options.any?
-        command.subcommands.each { |subcommand| display_help_for subcommand }
+        # Displays a very simple help screen for the given command and all its
+        # subcommands.
+        def display_help_for(command)
+          puts Template.for(command) if command.options.any?
+          command.subcommands.each { |subcommand| display_help_for subcommand }
+        end
+
+        private
+
+        # Adds a special help option to the given +command+.
+        #
+        # The last argument can be a configuration hash, which accepts the
+        # following options:
+        #
+        # [:switches]  The switches used when creating the help option.
+        def add_options_to!(*args)
+          opts, command = args.extract_ribbon!, args.first
+          switches = opts.switches? %w(-h --help)
+          description = opts.description? 'Show usage information and exit.'
+          command.option :acclaim_help, *switches, description
+        end
+
       end
 
     end
--- lib/acclaim/command/version.rb
+++ lib/acclaim/command/version.rb
@@ -6,40 +6,45 @@ module Acclaim
     # Module which adds version query support to a command.
     module Version
 
-      # Adds a special version option to the given +command+.
-      #
-      # The last argument can be a configuration hash, which accepts the
-      # following options:
-      #
-      # [:switches]  The switches used when creating the version option.
-      def self.add_options_to!(*args)
-        opts, command = args.extract_ribbon!, args.first
-        switches = opts.switches? %w(-v --version)
-        description = opts.description? 'Show version and exit.'
-        command.option :acclaim_version, *switches, description
-      end
+      # The class methods.
+      class << self
 
-      private_class_method :add_options_to!
-
-      # Creates a <tt>version</tt> subcommand that inherits from the given
-      # +base+ command and stores the class in the +Version+ constant of +base+.
-      # When called, the command displays the +version_string+ of the program
-      # and then exits.
-      #
-      # The last argument can be a configuration hash, which accepts the
-      # following options:
-      #
-      # [:options]   If +true+, will add a version option to the +base+ command.
-      # [:switches]  The switches used when creating the version option.
-      def self.create(*args)
-        opts, base, version_string = args.extract_ribbon!, args.first, args.last
-        add_options_to! base, opts if opts.options? true
-        base.const_set(:Version, Class.new(base)).tap do |version_command|
-          version_command.when_called do |options, args|
-            puts version_string
-            exit
+        # Creates a <tt>version</tt> subcommand that inherits from the given
+        # +base+ command and stores the class in the +Version+ constant of +base+.
+        # When called, the command displays the +version_string+ of the program
+        # and then exits.
+        #
+        # The last argument can be a configuration hash, which accepts the
+        # following options:
+        #
+        # [:options]   If +true+, will add a version option to the +base+ command.
+        # [:switches]  The switches used when creating the version option.
+        def create(*args)
+          opts, base, version_string = args.extract_ribbon!, args.first, args.last
+          add_options_to! base, opts if opts.options? true
+          base.const_set(:Version, Class.new(base)).tap do |version_command|
+            version_command.when_called do |options, args|
+              puts version_string
+              exit
+            end
           end
         end
+
+        private
+
+        # Adds a special version option to the given +command+.
+        #
+        # The last argument can be a configuration hash, which accepts the
+        # following options:
+        #
+        # [:switches]  The switches used when creating the version option.
+        def add_options_to!(*args)
+          opts, command = args.extract_ribbon!, args.first
+          switches = opts.switches? %w(-v --version)
+          description = opts.description? 'Show version and exit.'
+          command.option :acclaim_version, *switches, description
+        end
+
       end
 
     end
