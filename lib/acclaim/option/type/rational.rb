25e96ded2a2ed987915c335376f8347bc368097c
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-05-17 18:58:34 -0300

    Add Rational type.

---
+++ lib/acclaim/option/type/rational.rb
@@ -0,0 +1,21 @@
+require 'acclaim/option/type'
+
+module Acclaim
+  class Option
+    module Type
+
+      # Handles rational numbers given as arguments in the command line.
+      module Rational
+
+        # Simply returns +str.to_r+.
+        def self.handle(str)
+          str.to_r
+        end
+
+      end
+
+      self.accept ::Rational, &Rational.method(:handle)
+
+    end
+  end
+end
