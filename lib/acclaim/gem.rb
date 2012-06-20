ffcc0942081bf3a82e219741cd5b3a0256679bfb
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-06-20 15:31:20 -0300

    Add Acclaim::Gem class

---
+++ lib/acclaim/gem.rb
@@ -0,0 +1,15 @@
+module Acclaim
+
+  # Acclaim gem information and metadata.
+  #
+  # @author Matheus Afonso Martins Moreira
+  # @since 0.4.0
+  class Gem
+
+    root '../..'
+
+    specification ::Gem::Specification.load root.join('acclaim.gemspec').to_s
+
+  end
+
+end
