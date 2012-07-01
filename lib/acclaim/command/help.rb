8f0aa76781e6298f1c8cf832e975d1a783ea4c2d
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-06-30 23:38:42 -0300

    Don't take the arguments if they aren't going to be used

--- lib/acclaim/command/help.rb
+++ lib/acclaim/command/help.rb
@@ -27,7 +27,7 @@ module Acclaim
         options = Ribbon.wrap options
         Class.new(base_command).tap do |help_command|
           add_options_to! base_command, help_command, options if options.options? true
-          help_command.when_called do |options, arguments|
+          help_command.when_called do
             # TODO: implement a way to specify a command to the help option
             # and command.
             #   display_for options.command || args.pop
