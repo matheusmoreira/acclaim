66f4fe85ae73ddcbdfdbfcffe5c090f2c9632e10
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-07-10 21:20:24 -0300

    Add DSL::Help#description method
    Part of features requested in issue #1

--- lib/acclaim/command/dsl/help.rb
+++ lib/acclaim/command/dsl/help.rb
@@ -23,6 +23,32 @@ module Acclaim
           Command::Help.create root, arguments.extract_ribbon!, &block
         end
 
+        # This command's description.
+        #
+        # @overload description
+        #   Returns this command's description.
+        #
+        #   @return [String] this command's description
+        #
+        # @overload description(string)
+        #   Stores the given string as this command's description.
+        #
+        #   @param [#to_s] string the description
+        #   @return [String] the given description
+        #
+        # @overload description(&block)
+        #   Calls the given block to determine the description when needed.
+        #
+        #   @param [#call] block the block to call
+        #   @return [Proc] the given block
+        #   @yieldreturn [String] the description
+        def description(*arguments, &block)
+          if block.respond_to? :call then @description = block
+          elsif arguments.any? then @description = arguments.first.to_s
+          elsif @description.respond_to? :call then @description.call.to_s
+          else @description.to_s end
+        end
+
       end
     end
   end
