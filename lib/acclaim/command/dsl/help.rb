7c6cd334f5e0e151db9da5dd82bc1003e4a26d7f
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-07-10 21:30:54 -0300

    Add DSL::Help#note method, aliased as notice
    Part of features requested in issue #1

--- lib/acclaim/command/dsl/help.rb
+++ lib/acclaim/command/dsl/help.rb
@@ -49,6 +49,34 @@ module Acclaim
           else @description.to_s end
         end
 
+        # This command's usage notes.
+        #
+        # @overload note
+        #   Returns this command's usage notes.
+        #
+        #   @return [String] this command's usage notes
+        #
+        # @overload note(string)
+        #   Stores the given string as this command's usage notes.
+        #
+        #   @param [#to_s] string the notes
+        #   @return [String] the given notes
+        #
+        # @overload note(&block)
+        #   Calls the given block to determine the usage notes when needed.
+        #
+        #   @param [#call] block the block to call
+        #   @return [Proc] the given block
+        #   @yieldreturn [String] the usage notes
+        def note(*arguments, &block)
+          if block.respond_to? :call then @note = block
+          elsif arguments.any? then @note = arguments.first.to_s
+          elsif @note.respond_to? :call then @note.call.to_s
+          else @note.to_s end
+        end
+
+        alias notice note
+
       end
     end
   end
